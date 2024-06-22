556f5f7e8dd606e4cbb910a2774d82d3916dd67d
é. urcades <edouard@urcad.es>
2024-06-22 01:06:13 -0400

    Initial Commit

---
+++ Locale/LocaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocaleApp.swift
+//  Locale
+//
+//  Created by é. urcades on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct LocaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
